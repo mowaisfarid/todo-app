714753b801e42295c46a5c7508068f64a67a5f13
Muhammad zubair <mowaisfarid@gmail.com>
2021-02-01 17:40:38 +0500

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  var [todos,setTodos] = useState([])
+
+  var todo=useRef();
+  console.log("to is working",todo.current)
+
+function add(){
+  console.log("jafdsk")
+  
+  setTodos([...todos,todo.current.value])
+  console.log(todos)
+  todo.current.value = ''
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <input ref={todo}/>
+     <button onClick={add}>Add</button>
+     {todos.map((value,i)=>{
+       return(
+         <div key={i}>{value}</div>
+       )
+     })}
     </div>
   );
 }
